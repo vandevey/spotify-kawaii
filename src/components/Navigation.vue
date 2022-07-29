5fda307a545f6f50f464397409140f062195d16c
Yoan Vandevelde <yoan.vandevelde.isn@gmail.com>
2022-07-29 12:01:45 +0200

    ✨ add side panel

---
+++ src/components/Navigation.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="navigation">
+      <router-link to="/">Home</router-link>
+      <router-link to="/">Search</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+};
+</script>
