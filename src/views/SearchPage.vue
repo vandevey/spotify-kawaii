c7bfa638f54f543937ce5f1626dea16c596d25d9
Yoan Vandevelde <yoan.vandevelde.isn@gmail.com>
2022-08-03 21:10:43 +0200

    ✨ get results from API

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,28 +1,52 @@
 <template>
-  <div class="searchPage">{{myprop}}</div>
+  <div class="searchPage">{{ currentSearch }}</div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "SearchPage",
   components: {},
 
   data() {
     return {
-      myprop: "dqsdq"
-    }
+      currentSearch: "",
+    };
   },
 
-  mounted() {
-    this.myprop = "cfvygbuhin"
-  },
+  mounted() {},
 
   watch: {
     "$store.state.searchValue": {
       handler: function (nv) {
-        this.myprop = nv;
+        this.currentSearch = nv;
+       
+        if (this.currentSearch != "") {
+          this.search();
+        }
       },
       immediate: true,
+    },
+  },
+
+  methods: {
+    async search(item = this.currentSearch) {
+      console.log(item);
+      const parsedText = item.replace(/\s+/g, "+");
+      const search_url =
+        "https://api.spotify.com/v1/search?q=" +
+        parsedText +
+        "&type=track,artist,album&limit=20&market=FR&access_token=" +
+        this.$store.state.token;
+      axios
+        .get(search_url)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     },
   },
 };
